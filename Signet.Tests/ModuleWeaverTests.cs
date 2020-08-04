fb94a7cfa72a3165dc473769869c3b907fbed62d
周耀帅 <zys529@outlook.com>
2020-08-04 14:11:14 +0800

    完成单元测试；

--- Signet.Tests/ModuleWeaverTests.cs
+++ Signet.Tests/ModuleWeaverTests.cs
@@ -1,13 +1,32 @@
-﻿using System.Reflection;
+﻿using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
 using Fody;
 using Xunit;
 
 public sealed class ModuleWeaverTests
 {
     [Fact]
-    public void WithFields()
+    public void Attribute()
     {
-        var moduleWeaver = new ModuleWeaver();
-        var testResult = moduleWeaver.ExecuteTestRun("AssemblyToProcess.dll", ignoreCodes: new[] { "0x8013129D" });
+        // 使用 Signet.Tests 项目的版本信息
+        var moduleWeaver = new ModuleWeaver
+        {
+            Config = XElement.Parse("<Signet From=\"Git\" />"),
+            ProjectDirectoryPath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))),
+        };
+
+        var testResult = moduleWeaver.ExecuteTestRun("AssemblyToProcess.dll", runPeVerify: false, ignoreCodes: new[] { "0x8013129D" });
+
+        if (testResult.Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>() is AssemblyInformationalVersionAttribute attribute)
+        {
+            var version = attribute.InformationalVersion;
+
+            Assert.False(string.IsNullOrWhiteSpace(version), version);
+        }
+        else
+        {
+            Assert.True(default, $"未能在 AssemblyToProcess.dll 程序集上找到 AssemblyInformationalVersionAttribute 特性...");
+        }
     }
 }
